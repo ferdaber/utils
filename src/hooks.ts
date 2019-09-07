6e6e1b5413e0f3f31a61fe40c10de068aab81bf9
Ferdy Budhidharma <ferd312@gmail.com>
2019-09-07 15:40:57 -0400

    add async state

--- src/hooks.ts
+++ src/hooks.ts
@@ -9,7 +9,59 @@ import {
   SetStateAction,
   useLayoutEffect,
 } from 'react'
-import { iss } from 'js-utils'
+import { iss } from './js-utils'
+
+export type AsyncState<T> = {
+  loading: boolean
+  data?: T
+  lastLoaded?: Date
+  error?: any
+}
+
+/**
+ * useState wrapper that wraps an async state into its loading states and errors.
+ * When `getData` is called, `error` is set to `undefined`, and the last known `data` is kept.
+ */
+export function useAsyncState<T, A extends any[]>(
+  getData: (...args: A) => Promise<T>,
+  getDataArgs: A
+): AsyncState<T>
+export function useAsyncState<T>(getData: () => Promise<T>): AsyncState<T>
+export function useAsyncState<T, A extends any[]>(
+  getData: (...args: A) => Promise<T>,
+  getDataArgs: A = ([] as unknown) as A
+) {
+  const [state, setState] = useState<AsyncState<T>>({
+    loading: false,
+  })
+  useAsyncEffect(
+    async signal => {
+      setState(state => ({
+        data: state.data,
+        lastLoaded: state.lastLoaded,
+        loading: true,
+      }))
+      try {
+        const data = await getData.apply(null, getDataArgs)
+        if (signal.aborted) return
+        setState({
+          data,
+          lastLoaded: new Date(),
+          loading: false,
+        })
+      } catch (error) {
+        setState(state => ({
+          data: state.data,
+          lastLoaded: new Date(),
+          error,
+          loading: false,
+        }))
+      }
+    },
+    [getData, ...getDataArgs]
+  )
+  return state
+}
 
 /**
  * A useState wrapper that moves between a distinct list of states
